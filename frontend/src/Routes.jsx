edc9582eaa9a2183239be38827a20c006c8ebe8a
Your Actual Name <kentlabastida9@gmail.com>
2025-12-03 12:11:25 +0800

    Update Routes.jsx

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,18 +1,9 @@
 import { createBrowserRouter, Link } from "react-router";
 import Home from "./Pages/Home.jsx";
-import Login from "./Pages/Login.jsx";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <Home />,
-    },
-    {
-        path: "/login",
-        element: (
-            <Link to={"/"}>
-                <Login />
-            </Link>
-        ),
     },
 ], {basename: "/React-Portfolio"});
